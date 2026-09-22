1a907ef1774413b5dd941d6d1dd10a0b816ac453
Rael Solutions <raelsolutionsbr@gmail.com>
2026-09-22 17:20:32 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import Sidebar from "@/components/Sidebar";
 
 export const metadata = {
   title: "Rael Planner | Rael Solutions",
@@ -13,14 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body>
-        <div className="flex">
-          <Sidebar />
-          <main className="flex-1 md:ml-64 p-4 md:p-8 min-h-screen pt-16 md:pt-8">
-            {children}
-          </main>
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
